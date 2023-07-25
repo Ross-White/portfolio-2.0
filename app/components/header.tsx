469bc0eb8ac83d108d0012a21e0a029de62c823d
Ross White <ross.white@ecotricity.co.uk>
2023-07-25 11:22:14 +0100

    feat: early file structure and components

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <Link href="/">This is a link</Link>
+      </header>
+    </div>
+  );
+}
