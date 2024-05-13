80efcca8b30c1d74a95516afec309b8cc9e0798d
Ross White <ross.white@ecotricity.co.uk>
2024-05-13 16:19:45 +0100

    feat(stack): adds iam role

--- backend/lib/amplify-stack.ts
+++ backend/lib/amplify-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from '@aws-cdk/core';
+import * as iam from '@aws-cdk/aws-iam';
 import * as amplify from '@aws-cdk/aws-amplify';
 
 export class AmplifyStack extends cdk.Stack {
@@ -13,5 +14,11 @@ export class AmplifyStack extends cdk.Stack {
         oauthToken: cdk.SecretValue.secretsManager('github-token')
       })
     });
+
+    const amplifyDeploymentRole = new iam.Role(this, 'AmplifyRoleWebApp', {
+      assumedBy: new iam.ServicePrincipal('amplify.amazonaws.com'),
+      description: 'Custom role permitting resources creation from Amplify',
+    });
+    amplifyDeploymentRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'));
   }
 }
