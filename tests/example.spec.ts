b5d223cc27c204a4683022d17af923742656a7ff
Ross White <ross.white@ecotricity.co.uk>
2024-05-13 11:46:24 +0100

    feat(playwright): adds first assertion tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,28 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Ross White/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('About link', async ({ page }) => {
+  await page.goto('http://localhost:3000');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the About link.
+  await page.getByRole('link', { name: 'About' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to have a heading with the name of About.
+  await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+});
+
+test('CV link', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  // Click the CV link.
+  await page.getByRole('link', { name: 'CV' }).click();
+
+  // Expects page to have a heading with the name of CV.
+  await expect(page.getByRole('heading', { name: 'CV' })).toBeVisible();
 });
