fd383b2e3f27e44a9ac57daa22902088e7dd6c8e
Ross White <ross.white@ecotricity.co.uk>
2024-05-13 12:06:06 +0100

    feat(tests): adds home link test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -26,3 +26,13 @@ test('CV link', async ({ page }) => {
   // Expects page to have a heading with the name of CV.
   await expect(page.getByRole('heading', { name: 'CV' })).toBeVisible();
 });
+
+test('Home link', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  // Click the CV link.
+  await page.getByRole('link', { name: 'Home' }).click();
+
+  // Expects page to have a heading with the name of CV.
+  await expect(page.getByRole('heading', { name: 'Hello' })).toBeVisible();
+});
