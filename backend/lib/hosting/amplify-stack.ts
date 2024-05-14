c8e386130433ade497a8596fc4f65f97b4b0dbe6
Ross White <ross.white@ecotricity.co.uk>
2024-05-14 10:23:53 +0100

    fixes typo

--- backend/lib/hosting/amplify-stack.ts
+++ backend/lib/hosting/amplify-stack.ts
@@ -41,7 +41,7 @@ export class AmplifyStack extends cdk.Stack {
             },
           },
           artifacts: {
-            baseDirectory: 'frontent/.next',
+            baseDirectory: 'frontend/.next',
             files: ['**/*'],
           },
           cache: {
